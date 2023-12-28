b77b3ae74712d54ee826cbd917621f645f935a04
Ryazapov Kamil <ryazapov.kamil@inbox.ru>
2023-12-28 22:15:40 +0300

    add handler registration

--- bot.py
+++ bot.py
@@ -7,25 +7,27 @@ BOT_TOKEN = '5871731399:AAESNUPD2ZXDUgSi4-fwtL-3R0mWYQmmKTM'
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command(commands=['start']))
+
 async def start_command(message: Message) -> None:
     await message.answer(
         text='Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь'
         )
 
-@dp.message(Command(commands=['help']))
 async def help_command(message: Message) -> None:
     await message.answer(
         text='Напиши мне что-нибудь,'
         'а я в ответ пришлю тебе твоё сообщение'
     )
 
-@dp.message()
 async def send_echo(message: Message) -> None:
     await message.reply(
         text=message.text
     )
 
+
+dp.message.register(start_command, Command(commands=['start']))
+dp.message.register(help_command, Command(commands=['help']))
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot=bot)
